dc5c4c571308997e3ac5ddea4605e31c7a434ec8
besil <silvio.bernardinello@outlook.com>
2024-04-11 17:50:56 +0200

    to do: add README with quickstart

--- saltedge/services.py
+++ saltedge/services.py
@@ -3,6 +3,7 @@ from abc import ABC
 from saltedge.api.connections import ConnectionAPI
 from saltedge.api.countries import CountriesAPI
 from saltedge.api.lead.leads import LeadsAPI
+from saltedge.api.accounts import AccountAPI
 from saltedge.api.lead.sessions import LeadSessionAPI
 from saltedge.api.providers import ProviderAPI
 from saltedge.api.transactions import TransactionAPI
@@ -20,6 +21,7 @@ class BaseSaltedgeService(ABC):
         self.leads = LeadsAPI(client, self.base_url)
         self.lead_session = LeadSessionAPI(client, self.base_url)
         self.connections = ConnectionAPI(client, self.base_url)
+        self.accounts = AccountAPI(client, self.base_url)
         self.transactions = TransactionAPI(client, self.base_url)
 
 
